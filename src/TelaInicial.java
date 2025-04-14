611379ed653f9983f54d66534072c745652ec5ff
Abner Vital <avgchaves@gmail.com>
2025-04-14 06:50:20 -0300

    feat: menu funcional com opções de transações e saída

--- src/TelaInicial.java
+++ src/TelaInicial.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class TelaInicial {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String nomeCliente = "Abner Vital Gomes Chaves";
         String tipoConta = "Corrente";
         Double saldoInicial = 2500.00;
+        int opcao = 0;
         System.out.printf("""
                 ***********************************************
                 Dados Iniciais do Cliente:
@@ -12,5 +16,17 @@ public class TelaInicial {
                 Saldo Inicial: %.2f
                 ***********************************************
                 """,nomeCliente,tipoConta,saldoInicial);
+
+        while (opcao != 4){
+            System.out.printf("""
+                    Operações
+                    
+                    1- Consultar saldo
+                    2- Receber Valor
+                    3- Transferir
+                    4 - Sair
+                    """);
+            opcao = scanner.nextInt();
+        }
     }
 }
