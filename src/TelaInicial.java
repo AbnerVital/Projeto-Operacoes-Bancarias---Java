aaa41dc10984df83e9ad49244f204ece2eb92faa
Abner Vital <avgchaves@gmail.com>
2025-04-14 17:56:25 -0300

    feat: adicionar lógica para consultar saldo, receber e transferir valores

--- src/TelaInicial.java
+++ src/TelaInicial.java
@@ -5,7 +5,7 @@ public class TelaInicial {
         Scanner scanner = new Scanner(System.in);
         String nomeCliente = "Abner Vital Gomes Chaves";
         String tipoConta = "Corrente";
-        Double saldoInicial = 2500.00;
+        double saldoAtual = 2500.00;
         int opcao = 0;
         System.out.printf("""
                 ***********************************************
@@ -15,18 +15,43 @@ public class TelaInicial {
                 Tipo conta: %s
                 Saldo Inicial: %.2f
                 ***********************************************
-                """,nomeCliente,tipoConta,saldoInicial);
+                """, nomeCliente, tipoConta, saldoAtual);
 
-        while (opcao != 4){
-            System.out.printf("""
-                    Operações
-                    
-                    1- Consultar saldo
-                    2- Receber Valor
-                    3- Transferir
-                    4 - Sair
-                    """);
+        String menu = """
+                                                  \s
+                     Operações
+                    \s
+                     1- Consultar saldo
+                     2- Receber Valor
+                     3- Transferir
+                     4 - Sair
+                    \s
+                     Digite a opção desejada:
+                \s""";
+
+        while (opcao != 4) {
+            System.out.println(menu);
             opcao = scanner.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("O saldo atualizado é R$ " + saldoAtual);
+            } else if (opcao == 2) {
+                System.out.println("Informe o valor a receber:");
+                double valoraReceber = scanner.nextDouble();
+                saldoAtual += valoraReceber;
+                System.out.println("Saldo atualizado R$ " + saldoAtual);
+            } else if (opcao == 3) {
+                System.out.println("Informe o valor que deseja transferir:");
+                double valoraTransferir = scanner.nextDouble();
+                if (saldoAtual < valoraTransferir) {
+                    System.out.println("Não a saldo suficiente para fazer essa transferência.");
+                } else {
+                    saldoAtual -= valoraTransferir;
+                    System.out.println("Saldo atualizado R$ " + saldoAtual);
+                }
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida");
+            }
         }
     }
 }
